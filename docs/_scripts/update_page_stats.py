7df167d6d096e9f30f0b575a248728f5f42dec8e
David Dodel <david.dodel@hotmail.com>
2020-10-03 16:02:52 +0200

    Added docstrings and markdown support

--- docs/_scripts/update_page_stats.py
+++ docs/_scripts/update_page_stats.py
@@ -3,48 +3,61 @@ import logging
 import requests
 import argparse
 import pandas as pd
+import html2markdown as html2md
 from bs4 import BeautifulSoup
+from markdown import markdown as md2html
+
 
 # Files to update stats
-FILES = ["overview_stats.html", "dataset_stats_badges.html"]
+FILES = ["overview_stats.html", "../README.md"]
 box_stats_url = "https://drive.google.com/u/1/uc?id=1Om1iwSnJrJ1o_U9UGouVXuzWhkHYVEXf&export=download"
 
 
-def update_stats_badges(html_paths: [str], stats: dict):
+def update_stats_badges(md_paths: [str], stats: dict):
     """
+        Updates shield.io image tags according to stats and saves the changed Markdown files.
+        Matches the tags to be updated by their id attributes.
+        The id is equivalent to the stats dictionary key, whose value is used for updating.
     """
     updated_soups = []
-    for html_path in html_paths:
-        with open(html_path) as fp:
-            bs_badges = BeautifulSoup(fp, features="lxml")
+    for md_path in md_paths:
+        with open(md_path) as fp:
+            html = md2html(fp.read())
+            bs_badges = BeautifulSoup(html, features="lxml")
 
         for tag in bs_badges.find_all("img"):
-            if tag["id"] == "num_teams":
-                tag[
-                    "src"
-                ] = f"https://img.shields.io/badge/Teams-{stats['num_teams']:,}-green.svg"
-                tag["alt"] = f"Teams: {stats['num_teams']}"
-            elif tag["id"] == "num_bbox_images":
-                tag[
-                    "src"
-                ] = f"https://img.shields.io/badge/Images-{stats['num_bbox_images']:,}-blue.svg"
-                tag["alt"] = f"Amount of labeled images: {stats['num_bbox_images']}"
-            elif tag["id"] == "num_bbox_cones":
-                tag[
-                    "src"
-                ] = f"https://img.shields.io/badge/Cones-{stats['num_bbox_cones']:,}-blue.svg"
-                tag["alt"] = f"Amount of labeled cones: {stats['num_bbox_cones']}"
+            tag_id = tag.get("id")
+            if tag_id in stats.keys():
+                if tag["id"] == "num_teams":
+                    tag[
+                        "src"
+                    ] = f"https://img.shields.io/badge/Teams-{stats['num_teams']:,}-green.svg"
+                    tag["alt"] = f"Teams: {stats['num_teams']}"
+                elif tag["id"] == "num_bbox_images":
+                    tag[
+                        "src"
+                    ] = f"https://img.shields.io/badge/Images-{stats['num_bbox_images']:,}-blue.svg"
+                    tag["alt"] = f"Amount of labeled images: {stats['num_bbox_images']}"
+                elif tag["id"] == "num_bbox_cones":
+                    tag[
+                        "src"
+                    ] = f"https://img.shields.io/badge/Cones-{stats['num_bbox_cones']:,}-blue.svg"
+                    tag["alt"] = f"Amount of labeled cones: {stats['num_bbox_cones']}"
         updated_soups.append(bs_badges)
-    for stats_soup, html_path in zip(updated_soups, html_paths):
+    for stats_soup, md_path in zip(updated_soups, md_paths):
         logging.info(
-            f"Overwriting with current stats the following include file: {html_path}"
+            f"Overwriting with current stats the following markdown file: {md_path}"
         )
-        with open(html_path, "w") as stats_include:
-            stats_include.write(repr(stats_soup))
+        with open(md_path, "w") as stats_md:
+            markdown = html2md.convert(repr(stats_soup))
+            stats_md.write(markdown)
 
 
 def update_span_stats_pages(html_paths: [str], stats: dict):
     """
+        Updates _includes files that contain stats and saves the changed HTML files.
+        Matches span tags with id attributes to the stats dict.
+        Only updates if the id matches a key in the stats dict.
     """
     updated_soups = []
     for html_path in html_paths:
@@ -52,11 +65,16 @@ def update_span_stats_pages(html_paths: [str], stats: dict):
             bs_overview = BeautifulSoup(fp, features="lxml")
 
         for tag in bs_overview.find_all("span"):
-            if tag.get("id"):
+            tag_id = tag.get("id")
+            if tag_id in stats.keys():
                 if isinstance(stats[tag["id"]], float):
                     tag.string.replace_with(f"{stats[tag['id']]:6,.2f}")
                 else:
                     tag.string.replace_with(f"{stats[tag['id']]:,}")
+            elif tag_id:
+                logging.info(
+                    f"Found span tag with id and no matching stats dictionary entry for key: {tag['id']}."
+                )
         updated_soups.append(bs_overview)
     for stats_soup, html_path in zip(updated_soups, html_paths):
         logging.info(
@@ -91,9 +109,10 @@ def main():
     box_stats_df = pd.read_pickle("temp_box_stats.df")
 
     # The keys match the id's of the span tags to be filled
-    stats = {}
-    stats["num_bbox_images"] = len(pd.unique(box_stats_df["ann_file"]))
-    stats["num_bbox_cones"] = len(box_stats_df)
+    stats = {
+        "num_bbox_images": len(pd.unique(box_stats_df["ann_file"])),
+        "num_bbox_cones": len(box_stats_df),
+    }
     stats["avg_bbox_per_img"] = stats["num_bbox_cones"] / stats["num_bbox_images"]
     stats["num_teams"] = len(pd.unique(box_stats_df["team_name"]))
     stats["num_teams_data"] = len(
