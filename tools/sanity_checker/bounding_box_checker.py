63d1f0890792ceeb0ec24ee686aa2bd645f0c5c5
Niclas Vödisch <vniclas@ethz.ch>
2020-12-19 17:45:21 +0100

    Crop bounding boxes to main image area, i.e., without the watermark

--- tools/sanity_checker/bounding_box_checker.py
+++ tools/sanity_checker/bounding_box_checker.py
@@ -1,4 +1,5 @@
 import numpy as np
+import supervisely_lib as sly
 
 from similarity_scorer.utils.logger import Logger
 from .label_checker import LabelChecker
@@ -21,6 +22,7 @@ class BoundingBoxChecker(LabelChecker):
         is_ok &= not self._is_small_label(
             label, minimum_area=25, delete_threshold_area=10
         )
+        is_ok &= not self._is_outside_image_frame(label, image_border_size=140)
         return is_ok
 
     def _is_small_label(
@@ -41,3 +43,51 @@ class BoundingBoxChecker(LabelChecker):
         if remove_label:
             is_small_label = False
         return is_small_label
+
+    def _is_outside_image_frame(self, label: dict, image_border_size: int):
+        # Check if the label reaches into the black border (watermark)
+
+        corners = label["points"]["exterior"]
+        min_x = min(corners[0][0], corners[1][0])
+        max_x = max(corners[0][0], corners[1][0])
+        min_y = min(corners[0][1], corners[1][1])
+        max_y = max(corners[0][1], corners[1][1])
+
+        is_outside_image_frame = False
+        is_outside_image_frame |= min_x < image_border_size
+        is_outside_image_frame |= max_x > self.image_width - image_border_size
+        is_outside_image_frame |= min_y < image_border_size
+        is_outside_image_frame |= max_y > self.image_height - image_border_size
+
+        if not self.apply_auto_fixes:
+            self._update_issue_tag(label, "Inside watermark", is_outside_image_frame)
+        elif is_outside_image_frame:
+            # Crop bounding box to the main image
+            label["points"]["exterior"] = [
+                [max(min_x, image_border_size), max(min_y, image_border_size)],
+                [
+                    min(max_x, self.image_width - image_border_size),
+                    min(max_y, self.image_height - image_border_size),
+                ],
+            ]
+            self._update_rectangle_data(label)
+            # Remove issue tag if it previously existed
+            self._delete_issue_tag(label, "Inside watermark")
+
+        if self.verbose and is_outside_image_frame:
+            log_text = f"{self.image_name} | segmentation | inside watermark"
+            log_text += " --> fixed" if self.apply_auto_fixes else ""
+            Logger.log_info_alt(log_text)
+        if self.apply_auto_fixes:
+            is_outside_image_frame = False
+        return is_outside_image_frame
+
+    def _update_rectangle_data(self, label: dict):
+        updated_label = sly.Label.from_json(label, self.project_meta)
+        self._update_label(updated_label)
+        # Update label object for later checks
+        label = updated_label.to_json()
+        corner_points = label["points"]["exterior"]
+        label["area"] = np.abs(corner_points[0][0] - corner_points[1][0]) * np.abs(
+            corner_points[0][1] - corner_points[1][1]
+        )
