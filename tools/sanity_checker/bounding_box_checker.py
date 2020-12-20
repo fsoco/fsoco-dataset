04c4eadbfde75d3e251bd7c8dc782ce37a1dd32f
Niclas Vödisch <vniclas@ethz.ch>
2020-12-20 20:28:10 +0100

    Fix bounding box checker

--- tools/sanity_checker/bounding_box_checker.py
+++ tools/sanity_checker/bounding_box_checker.py
@@ -11,44 +11,44 @@ class BoundingBoxChecker(LabelChecker):
             raise ValueError(
                 f"Wrong label type: {label['geometryType']}. Expected: rectangle."
             )
+        # Run all checks on the same label.
+        self.label = label
 
         # Compute area of bounding box
-        corner_points = label["points"]["exterior"]
-        label["area"] = np.abs(corner_points[0][0] - corner_points[1][0]) * np.abs(
+        corner_points = self.label["points"]["exterior"]
+        self.label["area"] = np.abs(corner_points[0][0] - corner_points[1][0]) * np.abs(
             corner_points[0][1] - corner_points[1][1]
         )
 
         is_ok = True
-        is_ok &= not self._is_small_label(
-            label, minimum_area=25, delete_threshold_area=10
-        )
-        is_ok &= not self._is_outside_image_frame(label, image_border_size=140)
-        is_ok &= not self._is_distorted_box(label, minimum_ratio=0.9, maximum_ratio=2.5)
+        is_ok &= not self._is_small_label(minimum_area=20, delete_threshold_area=10)
+        is_ok &= not self._is_outside_image_frame(image_border_size=140)
+        is_ok &= not self._is_distorted_box(minimum_ratio=0.5, maximum_ratio=3.0)
         return is_ok
 
-    def _is_small_label(
-        self, label: dict, minimum_area: int, delete_threshold_area: int = -1
-    ):
-        is_small_label = label["area"] < minimum_area
-        remove_label = label["area"] < delete_threshold_area and self.apply_auto_fixes
+    def _is_small_label(self, minimum_area: int, delete_threshold_area: int = -1):
+        is_small_label = self.label["area"] < minimum_area
+        remove_label = (
+            self.label["area"] < delete_threshold_area and self.apply_auto_fixes
+        )
 
         if remove_label:
-            self._delete_label(label)
+            self._delete_label(self.label)
         else:
-            self._update_issue_tag(label, "Small label", is_small_label)
+            self._update_issue_tag(self.label, "Small label", is_small_label)
 
         if self.verbose and is_small_label:
-            log_text = f'{self.image_name} | bounding box | small label ({label["area"]} < {minimum_area})'
+            log_text = f'{self.image_name} | bounding box | small label ({self.label["area"]} < {minimum_area})'
             log_text += " --> removed" if remove_label else ""
             Logger.log_info_alt(log_text)
         if remove_label:
             is_small_label = False
         return is_small_label
 
-    def _is_outside_image_frame(self, label: dict, image_border_size: int):
+    def _is_outside_image_frame(self, image_border_size: int):
         # Check if the label reaches into the black border (watermark)
 
-        corner_points = label["points"]["exterior"]
+        corner_points = self.label["points"]["exterior"]
         min_x = min(corner_points[0][0], corner_points[1][0])
         max_x = max(corner_points[0][0], corner_points[1][0])
         min_y = min(corner_points[0][1], corner_points[1][1])
@@ -61,35 +61,35 @@ class BoundingBoxChecker(LabelChecker):
         is_outside_image_frame |= max_y > self.image_height - image_border_size
 
         if not self.apply_auto_fixes:
-            self._update_issue_tag(label, "Inside watermark", is_outside_image_frame)
+            self._update_issue_tag(
+                self.label, "Inside watermark", is_outside_image_frame
+            )
         elif is_outside_image_frame:
             # Crop bounding box to the main image
-            label["points"]["exterior"] = [
+            self.label["points"]["exterior"] = [
                 [max(min_x, image_border_size), max(min_y, image_border_size)],
                 [
                     min(max_x, self.image_width - image_border_size),
                     min(max_y, self.image_height - image_border_size),
                 ],
             ]
-            self._update_rectangle_data(label)
+            self._update_rectangle_data()
             # Remove issue tag if it previously existed
-            self._delete_issue_tag(label, "Inside watermark")
+            self._delete_issue_tag(self.label, "Inside watermark")
 
         if self.verbose and is_outside_image_frame:
-            log_text = f"{self.image_name} | segmentation | inside watermark"
+            log_text = f"{self.image_name} | bounding box | inside watermark"
             log_text += " --> fixed" if self.apply_auto_fixes else ""
             Logger.log_info_alt(log_text)
         if self.apply_auto_fixes:
             is_outside_image_frame = False
         return is_outside_image_frame
 
-    def _is_distorted_box(
-        self, label: dict, minimum_ratio: float, maximum_ratio: float
-    ):
+    def _is_distorted_box(self, minimum_ratio: float, maximum_ratio: float):
         # maximum_ratio: height to width
         # The reason for a distorted box could be that the box covers multiple labels
 
-        corner_points = label["points"]["exterior"]
+        corner_points = self.label["points"]["exterior"]
         box_width = np.abs(corner_points[0][0] - corner_points[1][0])
         box_height = np.abs(corner_points[0][1] - corner_points[1][1])
         ratio = box_height / box_width
@@ -97,7 +97,7 @@ class BoundingBoxChecker(LabelChecker):
         smaller_min_ratio = ratio < minimum_ratio
         is_distorted_box = greater_max_ratio or smaller_min_ratio
 
-        self._update_issue_tag(label, "Suspicious aspect ratio", is_distorted_box)
+        self._update_issue_tag(self.label, "Suspicious aspect ratio", is_distorted_box)
 
         if self.verbose and is_distorted_box:
             log_text = f"{self.image_name} | bounding box | aspect ratio ({np.round(ratio, 1)} "
@@ -108,12 +108,12 @@ class BoundingBoxChecker(LabelChecker):
             Logger.log_info_alt(log_text)
         return is_distorted_box
 
-    def _update_rectangle_data(self, label: dict):
-        updated_label = sly.Label.from_json(label, self.project_meta)
+    def _update_rectangle_data(self):
+        updated_label = sly.Label.from_json(self.label, self.project_meta)
         self._update_label(updated_label)
         # Update label object for later checks
-        label = updated_label.to_json()
-        corner_points = label["points"]["exterior"]
-        label["area"] = np.abs(corner_points[0][0] - corner_points[1][0]) * np.abs(
+        self.label = updated_label.to_json()
+        corner_points = self.label["points"]["exterior"]
+        self.label["area"] = np.abs(corner_points[0][0] - corner_points[1][0]) * np.abs(
             corner_points[0][1] - corner_points[1][1]
         )
