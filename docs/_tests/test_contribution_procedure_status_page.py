597484a64447f3b22b2682e7833861324e3da47a
David Dodel <david.dodel@hotmail.com>
2022-04-10 12:48:35 +0200

    Simplify test via response text without rendering

--- docs/_tests/test_contribution_procedure_status_page.py
+++ docs/_tests/test_contribution_procedure_status_page.py
@@ -56,18 +56,12 @@ def get_teams() -> List[str]:
 def test_google_app_script_response():
     session = HTMLSession()
     teams = get_teams()
-    responses = []
+    response_status = []
     for team in teams:
         logging.info(f"Getting page for {team}")
         r = session.get(GAPPS_URL, params={"team_name": team})
-        r.html.render(sleep=4, timeout=15.0)
-        responses.append(r.html.text)
-    # render for some reason doesn't execute the GApp JS script correctly
-    # The User HTML generated can be searched through regex though
-    # "Success" => team_name found; "Fail" => team_name not found
-    response_status = [
-        True if response.find("Success") >= 0 else False for response in responses
-    ]
+        status = True if r.find(team) >= 0 else False
+        response_status.append(status)
     assert all(response_status), [
         team_name for team_name, res in zip(teams, response_status) if not res
     ]
